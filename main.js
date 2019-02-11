6ef0138896e5fcca62f4ccc92330f011d0acaa0a
CAH1983 <carolineho1983@yahoo.fr>
2019-02-11 13:53:05 +0000

    fixed the function ^-^ yay

--- main.js
+++ main.js
@@ -1,61 +1,67 @@
-let todos = ["buy new turtle"];
-
-let input = prompt("Enter type of action to perform 🤖 : New, Delete, List, Quit ")
-
-// ----------------------- functions ----------------------
-function listToDos() {
-    console.log('**************');
+document.addEventListener("DOMContentLoaded", function (event) {
+    //do work
     
-    todos.forEach(function(todo, i){
-        console.log(i + ': ' + todo);
-    });
-    console.log('**************');
-}
-
-function addTodo() {
-    // ask for a new todo
-    let newTodo = prompt("Enter new Todo");
-    // add to the todos array
-    todos.push(newTodo);
-}
-
-function deleteTodo() {
-    //ask for index of todo to be deleted
-    var index = prompt('Enter index of todo to delete');
-    // delete that todo
-    todos.splice(index, 1)
-    console.log('Deleted to do');
-}
-
-// ------------- loop prompt -------------------
-while(input !== "quit") {
-    // handle input
-    if(input === "list") {
-        listToDos();   
-    } else if (input === "new") {
-        addTodo();
-    } else if (input === "delete") {
-        deleteTodo();
+    let todos = ["buy new turtle"];
+    
+    let input = prompt("Enter type of action to perform 🤖 : New, Delete, List, Quit ")
+    
+    // ----------------------- functions ----------------------
+    function listToDos() {
+        console.log('**************');
+        
+        todos.forEach(function(todo, i){
+            console.log(i + ': ' + todo);
+        });
+        console.log('**************');
     }
     
-    //ask again for new input
-    input = prompt("Enter type of action to perform 🤖 : New, Delete, List, Quit ");
-}
-console.log("OK, you quit the app")
-
-
-// ---------------------------------- to debug  
-const newTodoBtn = document.getElementById("#newTodoBtn");
-let isBlue = false;
-
-// changes color of the box
-setInterval(function() {
-    if(isBlue) {
-        newTodoBtn.style.background="white";
-    } else {
-        newTodoBtn.style.background="blue";
+    function addTodo() {
+        // ask for a new todo
+        let newTodo = prompt("Enter new Todo");
+        // add to the todos array
+        todos.push(newTodo);
     }
-    isBlue = !isBlue;
+    
+    function deleteTodo() {
+        //ask for index of todo to be deleted
+        var index = prompt('Enter index of todo to delete');
+        // delete that todo
+        todos.splice(index, 1)
+        console.log('Deleted to do');
+    }
+    
+    // ------------- loop prompt -------------------
+    while(input !== "quit") {
+        // handle input
+        if(input === "list") {
+            listToDos();   
+        } else if (input === "new") {
+            addTodo();
+        } else if (input === "delete") {
+            deleteTodo();
+        }
+        
+        //ask again for new input
+        input = prompt("Enter type of action to perform 🤖 : New, Delete, List, Quit ");
+    }
+    console.log("OK, you quit the app")
+    
+    
+    // ---------------------------------- to debug  
+    const newTodoBtn = document.querySelector("#newTodoBtn");
+    let isBlue = false;
+    
+    // changes color of the box
+    newTodoBtn.style.color="blue";
+    
+    setInterval(function() {
+        if(isBlue) {
+            newTodoBtn.style.background="white";
+        } else {
+            newTodoBtn.style.background="blue";
+        }
+        isBlue = !isBlue;
     }, 1000);
-
-console.log(newTodoBtn);
+    
+    console.log(newTodoBtn);
+});
