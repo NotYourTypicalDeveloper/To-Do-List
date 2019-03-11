340c90c8bc4aabace6d254c45645114f87d93d8a
CAH1983 <carolineho1983@yahoo.fr>
2019-03-11 21:37:57 +0000

    refactored fn using toggleClass

--- assets/JS/todos.js
+++ assets/JS/todos.js
@@ -2,19 +2,6 @@ console.log('connected');
 
 //check off specific todos by clicking
 $('li').click(function(){
-    // if li is gray. Be careful to use the rgb color reference otherwise it won't work if we use the simple color name.
-    if($(this).css('color') === 'rgb(128, 128, 128)') {
-        $(this).css({
-            color: 'black',
-            textDecoration: 'none'           
-        });
-    } 
-    
-    else {
-        $(this).css({
-            color: 'gray',
-            textDecoration: 'line-through'
-        });
-    }
+  $(this).toggleClass('completed');
 });
 
