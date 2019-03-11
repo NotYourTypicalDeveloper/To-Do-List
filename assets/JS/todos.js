8b6136743578fc6dd35995c8bd2287d0d9d012a5
CAH1983 <carolineho1983@yahoo.fr>
2019-03-11 21:33:46 +0000

    added strike-through function

--- assets/JS/todos.js
+++ assets/JS/todos.js
@@ -2,6 +2,19 @@ console.log('connected');
 
 //check off specific todos by clicking
 $('li').click(function(){
-    alert('clicked LI');
-     $(this).css('color', 'gray');
+    // if li is gray. Be careful to use the rgb color reference otherwise it won't work if we use the simple color name.
+    if($(this).css('color') === 'rgb(128, 128, 128)') {
+        $(this).css({
+            color: 'black',
+            textDecoration: 'none'           
+        });
+    } 
+    
+    else {
+        $(this).css({
+            color: 'gray',
+            textDecoration: 'line-through'
+        });
+    }
 });
+
