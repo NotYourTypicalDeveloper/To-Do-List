a002258c25ab9be71bf8ff3c641158bd0ebf8de8
CAH1983 <carolineho1983@yahoo.fr>
2019-03-12 17:39:21 +0000

    changed click to onclick method

--- assets/JS/todos.js
+++ assets/JS/todos.js
@@ -1,15 +1,25 @@
 console.log('connected');
 
 //check off specific todos by clicking
-$('li').click(function(){
+$('ul').on('click', 'li', function(){
   $(this).toggleClass('completed');
 });
 
 //click on X to delete Todo
-$('span').click(function(event) {
+$('ul').on('click', 'span', function(event) {
+    $(this).parent().fadeOut(300, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
 
-$(this).parent().remove();
-event.stopPropagation();
-
-
+$("input[type='text']").keypress(function(event) {
+  if(event.which === 13) {
+    //grab new todo text from the input
+    let todoText = $(this).val();
+    $(this).val('');
+    // create a new li and add it to the ul
+    $('ul').append('<li><span>X </span>' + todoText + '</li>');
+  }
+  
 });
