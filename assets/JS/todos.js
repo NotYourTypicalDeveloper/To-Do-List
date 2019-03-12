9f5b05db7842ea787090eb5db0c1430456466cee
CAH1983 <carolineho1983@yahoo.fr>
2019-03-12 15:09:15 +0000

    added fn delete todo

--- assets/JS/todos.js
+++ assets/JS/todos.js
@@ -5,3 +5,11 @@ $('li').click(function(){
   $(this).toggleClass('completed');
 });
 
+//click on X to delete Todo
+$('span').click(function(event) {
+
+$(this).parent().remove();
+event.stopPropagation();
+
+
+});
